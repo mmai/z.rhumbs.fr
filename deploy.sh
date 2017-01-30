1a183cfbfdf040796814afea08bed84885702d36
Henri Bourcereau <henri.bourcereau@gmail.com>
2017-01-30 11:45:52 +0100

    clean publish

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,22 @@
 #Position on current develop branch
-git stash
 git checkout develop
+if [ -z "$(git status --porcelain)" ]; then
+  #Build new files
+  stack exec site clean
+  stack exec site build
 
-#Build new files
-stack exec site clean
-stack exec site build
+  #Prepare new files
+  git fetch --all
+  git checkout master
+  cp -a _site/. .
+  git add .
 
-#Prepare new files
-git fetch --all
-git checkout master
-cp -a _site/. .
-git add .
+  #Push to github
+  git commit -m "publish"
+  git push
 
-#Push to github
-git commit -m "publish"
-git push
-
-# Restoration
-git checkout develop
-git stash pop
+  # Restoration
+  git checkout develop
+else 
+  echo "COULD NOT PUBLISH : commit all your changes, or stash them"
+fi 
